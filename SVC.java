99606fd08b1b017d4a1e3b9a3ad264ce132c395b
Sanya Srivastava <129599279+Sanya0732@users.noreply.github.com>
2024-01-14 17:47:22 +0530

    Add files via upload

---
+++ SVC.java
@@ -0,0 +1,41 @@
+
+public class SVC {
+
+	public static void main(String[] args) {
+	   byte age=10;
+	   byte johnAge=10;
+	   
+	   age=22;
+	   
+	   short fionnasAge=30;
+	   
+	   int numberofstudents=30;
+	   
+	   long phonenumber=9876543789l;
+	   
+	   int myAge=fionnasAge;
+	   byte yourage = (byte)myAge;
+	   
+	   System.out.println("Your age:"+yourage);
+	   
+	   float pi = 3.14f;
+	   
+	   double tax = 22.12;
+	   
+	   char ch='A';
+	   System.out.println("1.Ch is :"+ch);
+	   
+	    ch=66;
+	   System.out.println("2.ch is :"+ch);
+	   
+	   ch = '\u03A9';
+	   System.out.println("3 ch is:"+ch);
+	   
+	 boolean internet= true;  
+	 System.out.println("Is internet connected?"+internet);
+	   
+	   
+
+	}
+
+}
